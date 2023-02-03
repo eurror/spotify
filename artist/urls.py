1a37518b30f060aec17d17309dd0605afde985ca
evr <edgarpo0401@gmail.com>
2023-02-03 18:02:46 +0600

    added MVC for artist app

--- artist/urls.py
+++ artist/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('artists/', views.ArtistListView.as_view()),
+    path('artists/<int:pk>/', views.ArtistDetailView.as_view()),
 ]
--- artist/views.py
+++ artist/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+
+from .models import Artist
+from .serializers import ArtistSerializer
+
+class ArtistListView(generics.ListAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class ArtistDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            artist = Artist.objects.get(pk=pk)
+            serializer = ArtistSerializer(instance=artist)
+            return Response(serializer.data, status=200)
+        except Artist.DoesNotExist:
+            return Response('This artist doesn\'t exist', status=404)

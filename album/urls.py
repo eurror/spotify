ed7f6dd693f58c03242e0f1a46b7364ffb0a2d7a
evr <edgarpo0401@gmail.com>
2023-02-03 18:26:35 +0600

    added MVC for album app

--- album/urls.py
+++ album/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('albums/', views.AlbumListView.as_view()),
+    path('albums/<int:pk>/', views.AlbumDetailView.as_view()),
 ]
--- album/views.py
+++ album/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Album
+from .serializers import AlbumSerializer
+
+class AlbumListView(generics.ListAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class AlbumDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            album = Album.objects.get(pk=pk)
+            serializer = AlbumSerializer(instance=album)
+            return Response(serializer.data, status=200)
+        except Album.DoesNotExist:
+            return Response('This album doesn\'t exist', status=404)
